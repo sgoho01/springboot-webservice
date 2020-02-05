3fab1cab6d3a6b499584dcdafbf0f958142feaf1
ghsong <ghsong@greenlamp.kr>
2020-02-05 14:31:38 +0900

    테스트코드 : 임포트 수정

--- src/test/java/com/ghsong/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ghsong/springboot/web/HelloControllerTest.java
@@ -6,9 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,8 +36,8 @@ public class HelloControllerTest {
                 .param("name", name)
                 .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
-                //.andExpect(jsonPath("$.name", is(name)))
-                //.andExpect(jsonPath("$.amount", is(amount)))
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)))
         ;
     }
 
