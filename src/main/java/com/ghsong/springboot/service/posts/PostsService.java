943c200ccf244ae2d3aa053e86f54505139f4c08
ghsong <ghsong@greenlamp.kr>
2020-02-06 18:29:34 +0900

    Entity Dto 생성 & Posts 등록하는 Api/Test 생성

--- src/main/java/com/ghsong/springboot/service/posts/PostsService.java
+++ src/main/java/com/ghsong/springboot/service/posts/PostsService.java
@@ -1,4 +1,21 @@
 package com.ghsong.springboot.service.posts;
 
+import com.ghsong.springboot.domain.posts.PostsRepository;
+import com.ghsong.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
 }
