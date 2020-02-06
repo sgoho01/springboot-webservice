49604c56d024db3664fc5998168f11ae2385815d
ghsong <ghsong@greenlamp.kr>
2020-02-06 18:54:13 +0900

    Posts 수정, 조회 API 추가

--- src/main/java/com/ghsong/springboot/service/posts/PostsService.java
+++ src/main/java/com/ghsong/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.ghsong.springboot.service.posts;
 
+import com.ghsong.springboot.domain.posts.Posts;
 import com.ghsong.springboot.domain.posts.PostsRepository;
+import com.ghsong.springboot.web.dto.PostsResponseDto;
 import com.ghsong.springboot.web.dto.PostsSaveRequestDto;
+import com.ghsong.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 
 }
