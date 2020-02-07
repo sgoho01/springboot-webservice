8f238072d88ba990cd29841f1e12aec884914e32
ghsong <ghsong@greenlamp.kr>
2020-02-07 11:52:46 +0900

    Posts 조회 화면 추가 & JPA Query로 조회방법 추가

--- src/main/java/com/ghsong/springboot/service/posts/PostsService.java
+++ src/main/java/com/ghsong/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.ghsong.springboot.service.posts;
 
 import com.ghsong.springboot.domain.posts.Posts;
 import com.ghsong.springboot.domain.posts.PostsRepository;
+import com.ghsong.springboot.web.dto.PostsListResponseDto;
 import com.ghsong.springboot.web.dto.PostsResponseDto;
 import com.ghsong.springboot.web.dto.PostsSaveRequestDto;
 import com.ghsong.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +39,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDEesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
