8e078e3f0c4b54aa58c5ea27480ce39c858322d4
ghsong <ghsong@gmail.com>
2020-03-03 15:49:34 +0900

    무중단배포 스크립트 수정

--- script/start.sh
+++ script/start.sh
@@ -10,16 +10,16 @@ PROJECT_NAME=springboot-service
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp ${REPOSITORY}/zip/*.jar ${REPOSITORY}/
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr ${REPOSITORY}/*.jar | tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x ${JAR_NAME}
 
 echo "> $JAR_NAME 실행"
 
